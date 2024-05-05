4172ef0e2c00efa1be82c890c6b2920615d93d17
Vaen Code <marlo.sulca@tecsup.edu.pe>
2024-05-05 13:45:02 -0500

    PlayerControles_Modified

---
+++ Assets/Scripts/Game/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerMovement : MonoBehaviour
+{
+   
+    private Rigidbody2D myRB;
+    [SerializeField]
+    private float speed;
+    private float limitSuperior;
+    private float limitInferior;
+    public int player_lives = 4;
+    private Vector2 _movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+        SetMinMax();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_movement.y > 0 && transform.position.y < limitSuperior)
+        {
+            myRB.velocity = new Vector2(0f, speed);
+        }
+        else if (_movement.y < 0 && transform.position.y > limitInferior)
+        {
+            myRB.velocity = new Vector2(0f, -speed);
+        }
+        else
+        {
+            myRB.velocity = Vector2.zero;
+        }
+    }
+
+    void SetMinMax()
+    {
+        Vector3 bounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        limitInferior = -bounds.y;
+        limitSuperior = bounds.y;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Candy")
+        {
+            CandyGenerator.instance.ManageCandy(other.gameObject.GetComponent<CandyController>(), this);
+        }
+    }
+
+    public void Movement(InputAction.CallbackContext context)
+    {
+        _movement = context.ReadValue<Vector2>();
+    }
+}
